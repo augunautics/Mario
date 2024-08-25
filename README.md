df930ee54fec262df8a76a47b8c8deec699b0e1e
augunautics <39922636+augunautics@users.noreply.github.com>
2024-08-25 07:17:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mario
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/augunautics/Mario)
---
+++ main.js
@@ -0,0 +1,2 @@
+// This script sets the content of the div with id 'message' to "Hello, Mario"
+document.getElementById('message').textContent = 'Hello, Mario';
